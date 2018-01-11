abaa3479de49354fbe0ba9605e2ae43456dc7e97
joeldudleyr3 <joel.dudley@r3.com>
2018-01-11 13:19:22 +0000

    Refactors and adds comments to the flows.

--- cordapp/src/main/kotlin/com/template/Flows.kt
+++ cordapp/src/main/kotlin/com/template/Flows.kt
@@ -9,6 +9,7 @@ import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import net.corda.core.utilities.ProgressTracker
 
 object ProposalFlowPair {
     enum class Role { Buyer, Seller }
@@ -16,21 +17,36 @@ object ProposalFlowPair {
     @InitiatingFlow
     @StartableByRPC
     class ProposeTrade(val role: Role, val amount: Int, val counterparty: Party) : FlowLogic<Unit>() {
+        override val progressTracker = ProgressTracker()
+
         @Suspendable
         override fun call() {
-            val output = when (role) {
-                Role.Buyer -> ProposalState(amount, ourIdentity, counterparty, ourIdentity, counterparty)
-                Role.Seller -> ProposalState(amount, counterparty, ourIdentity, ourIdentity, counterparty)
+            // Creating the output.
+            val (buyer, seller) = when (role) {
+                Role.Buyer -> ourIdentity to counterparty
+                Role.Seller -> counterparty to ourIdentity
             }
-            val requiredSigners = listOf(ourIdentity.owningKey, counterparty.owningKey)
-            val command = Command(ProposalAndTradeContract.Commands.Propose(), requiredSigners)
+            val output = ProposalState(amount, buyer, seller, ourIdentity, counterparty)
 
+            // Creating the command.
+            val commandType = ProposalAndTradeContract.Commands.Propose()
+            val requiredSigners = listOf(ourIdentity.owningKey, counterparty.owningKey)
+            val command = Command(commandType, requiredSigners)
+
+            // Building the transaction.
             val notary = serviceHub.networkMapCache.notaryIdentities.first()
-            val txBuilder = TransactionBuilder(notary).addOutputState(output, ProposalAndTradeContract.ID).addCommand(command)
+            val txBuilder = TransactionBuilder(notary)
+            txBuilder.addOutputState(output, ProposalAndTradeContract.ID)
+            txBuilder.addCommand(command)
+
+            // Signing the transaction ourselves.
             val partStx = serviceHub.signInitialTransaction(txBuilder)
 
+            // Gathering the counterparty's signature.
             val counterpartySession = initiateFlow(counterparty)
             val fullyStx = subFlow(CollectSignaturesFlow(partStx, listOf(counterpartySession)))
+
+            // Finalising the transaction.
             subFlow(FinalityFlow(fullyStx))
         }
     }
@@ -41,7 +57,7 @@ object ProposalFlowPair {
         override fun call() {
             subFlow(object : SignTransactionFlow(counterpartySession) {
                 override fun checkTransaction(stx: SignedTransaction) {
-                    // TODO: Do some checking here.
+                    // No checking to be done.
                 }
             })
         }
@@ -52,22 +68,38 @@ object AcceptanceFlowPair {
     @InitiatingFlow
     @StartableByRPC
     class AcceptProposal(val proposalId: UniqueIdentifier) : FlowLogic<Unit>() {
+        override val progressTracker = ProgressTracker()
+
         @Suspendable
         override fun call() {
+            // Retrieving the input from the vault.
             val inputCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(proposalId))
             val inputStateAndRef = serviceHub.vaultService.queryBy<ProposalState>(inputCriteria).states.single()
             val input = inputStateAndRef.state.data
+
+            // Creating the output.
             val output = TradeState(input.amount, input.buyer, input.seller, input.linearId)
+
+            // Creating the command.
             val requiredSigners = listOf(input.proposer.owningKey, input.proposee.owningKey)
             val command = Command(ProposalAndTradeContract.Commands.Accept(), requiredSigners)
 
+            // Building the transaction.
             val notary = inputStateAndRef.state.notary
-            val txBuilder = TransactionBuilder(notary).addInputState(inputStateAndRef).addOutputState(output, ProposalAndTradeContract.ID).addCommand(command)
+            val txBuilder = TransactionBuilder(notary)
+            txBuilder.addInputState(inputStateAndRef)
+            txBuilder.addOutputState(output, ProposalAndTradeContract.ID)
+            txBuilder.addCommand(command)
+
+            // Signing the transaction ourselves.
             val partStx = serviceHub.signInitialTransaction(txBuilder)
 
+            // Gathering the counterparty's signature.
             val counterparty = serviceHub.identityService.requireWellKnownPartyFromAnonymous(input.proposer)
             val counterpartySession = initiateFlow(counterparty)
             val fullyStx = subFlow(CollectSignaturesFlow(partStx, listOf(counterpartySession)))
+
+            // Finalising the transaction.
             subFlow(FinalityFlow(fullyStx))
         }
     }
@@ -93,22 +125,38 @@ object ModificationFlowPair {
     @InitiatingFlow
     @StartableByRPC
     class ModifyProposal(val proposalId: UniqueIdentifier, val newAmount: Int) : FlowLogic<Unit>() {
+        override val progressTracker = ProgressTracker()
+
         @Suspendable
         override fun call() {
+            // Retrieving the input from the vault.
             val inputCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(proposalId))
             val inputStateAndRef = serviceHub.vaultService.queryBy<ProposalState>(inputCriteria).states.single()
             val input = inputStateAndRef.state.data
+
+            // Creating the output.
             val counterparty = serviceHub.identityService.requireWellKnownPartyFromAnonymous(input.proposer)
             val output = input.copy(amount = newAmount, proposer = ourIdentity, proposee = counterparty)
+
+            // Creating the command.
             val requiredSigners = listOf(input.proposer.owningKey, input.proposee.owningKey)
             val command = Command(ProposalAndTradeContract.Commands.Modify(), requiredSigners)
 
+            // Building the transaction.
             val notary = inputStateAndRef.state.notary
-            val txBuilder = TransactionBuilder(notary).addInputState(inputStateAndRef).addOutputState(output, ProposalAndTradeContract.ID).addCommand(command)
+            val txBuilder = TransactionBuilder(notary)
+            txBuilder.addInputState(inputStateAndRef)
+            txBuilder.addOutputState(output, ProposalAndTradeContract.ID)
+            txBuilder.addCommand(command)
+
+            // Signing the transaction ourselves.
             val partStx = serviceHub.signInitialTransaction(txBuilder)
 
+            // Gathering the counterparty's signature.
             val counterpartySession = initiateFlow(counterparty)
             val fullyStx = subFlow(CollectSignaturesFlow(partStx, listOf(counterpartySession)))
+
+            // Finalising the transaction.
             subFlow(FinalityFlow(fullyStx))
         }
     }
